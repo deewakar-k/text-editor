13673937a29e47c5b1043e542c2ccc4e01c50b0f
deewakar-k <dakshftw01@gmail.com>
2024-08-15 21:20:24 +0530

    styling

--- src/components/tiptap.jsx
+++ src/components/tiptap.jsx
@@ -1,3 +1,4 @@
+import { RiBold, RiH1, RiH2, RiItalic } from '@remixicon/react'
 import Highlight from '@tiptap/extension-highlight'
 import Placeholder from '@tiptap/extension-placeholder'
 import Typography from '@tiptap/extension-typography'
@@ -13,29 +14,39 @@ function Tiptap() {
       Highlight,
       Typography,
       Placeholder.configure({
-        placeholder: 'Tell your story...'
+        placeholder: 'Tell your story...',
+        emptyEditorClass: 'is-editor-empty',
+        emptyNodeClass: 'is-empty'
       })
     ],
+    content: '<h1></h1>',
+    editorProps: {
+      attributes: {
+        class: 'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
+      }
+    }
   })
 
   return (
     <>
-      {editor && <BubbleMenu className="bubble-menu" tippyOptions={{ duration: 100 }} editor={editor}>
+      {editor && <BubbleMenu className="bubble-menu bg-black rounded-md pt-[2px] text-white" tippyOptions={{ duration: 100 }} editor={editor}>
         <button
           onClick={() => editor.chain().focus().toggleBold().run()}
-          className={editor.isActive('bold') ? 'is-active' : ''}
+          className={editor.isActive('bold') ? 'is-active text-green-200' : ''}
         >
-          Bold
+          <RiBold size={20} />
         </button>
+        <span className='seperator'></span>
         <button
           onClick={() => editor.chain().focus().toggleItalic().run()}
           className={editor.isActive('italic') ? 'is-active' : ''}
         >
-          Italic
+          <RiItalic size={20} />
         </button>
+        <span className='seperator'></span>
         <button onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-          className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}>
-          T
+          className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}>
+          <RiH2 size={20} />
         </button>
       </BubbleMenu >}
 
